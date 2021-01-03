6f130cfb2396b7108cc341fe1b3a79417b632e19
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-03 19:22:43 +0200

    added pundit to bond

---
+++ app/policies/bond_policy.rb
@@ -0,0 +1,35 @@
+class BondPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+end
