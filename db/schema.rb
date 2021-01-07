d0b26eaeda3e07224705ff8ccc5117f738968afc
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-07 14:46:50 +0200

    adde migrations to change column data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_213805) do
+ActiveRecord::Schema.define(version: 2021_01_07_124400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_213805) do
   end
 
   create_table "cash_withdrawals", force: :cascade do |t|
-    t.integer "amount"
+    t.float "amount"
     t.string "reason"
     t.bigint "cash_id", null: false
     t.datetime "created_at", precision: 6, null: false
