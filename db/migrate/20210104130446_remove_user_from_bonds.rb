e7c3d1f44564a4a8398766657edb188575cf372d
hazim0001 <hazim_alsayed@hotmail.com>
2021-01-17 00:25:44 +0200

    remove ref

--- db/migrate/20210104130446_remove_user_from_bonds.rb
+++ db/migrate/20210104130446_remove_user_from_bonds.rb
@@ -1,6 +1,6 @@
 class RemoveUserFromBonds < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :bonds, :user, null: false, foreign_key: true
+    # remove_reference :bonds, :user, null: false, foreign_key: true
     add_reference :bonds, :asset, index: true
   end
 end
